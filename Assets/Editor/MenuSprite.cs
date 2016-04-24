f605a4ffbe68e89c2d34ab1a10d45a9a5a2a6afa
lichengyu <lichengyu1985@163.com>
2016-04-24 23:10:16 +0800

    human

--- Assets/Editor/MenuSprite.cs
+++ Assets/Editor/MenuSprite.cs
@@ -102,3 +102,87 @@ public class MenuSprite {
 	}
 }
 
+public class ImportHuman {
+	const int SPRITE_SIZE = 16;
+	const string tilesetPath = "/Resources/Sprites/human.png";
+	private static SpriteMetaData Def(int y, int x, string name, int addx) {
+		int px = addx + x * (SPRITE_SIZE + 1);
+		int py = (11-y) * (SPRITE_SIZE + 1);
+		SpriteMetaData meta = new SpriteMetaData();
+		meta.name = name;
+		meta.rect.xMin = px;
+		meta.rect.yMin = py;
+		meta.rect.xMax = px + SPRITE_SIZE;
+		meta.rect.yMax = py + SPRITE_SIZE;
+		meta.alignment = 0;
+		return meta;
+	}
+	private static SpriteMetaData Def0(int y, int x, string name) {
+		return Def(y, x, name, 0);
+	}
+	private static SpriteMetaData Def1(int y, int x, string name) {
+		return Def(y, x, name, 1);
+	}
+	private static void AddDef(List<SpriteMetaData> metas, string prefix, 
+		int x0, int y0, int n1, int x1, int y1, int n2, int x2) {
+		for (int i2 = 0, i = 0; i2 < n2; ++i2) {
+			for (int i1 = 0; i1 < n1; ++i1) {
+				int y = y0 + i1 / x1 + i2 / x2 * y1;
+				int x = x0 + i1 % x1 + i2 % x2 * x1;
+				metas.Add(Def1(y, x, prefix + i));
+			}
+		}
+	}
+	[MenuItem("Revenge/Import Human Sprites", priority = 200)]
+	private static void PackSprites() {
+		List<SpriteMetaData> metas = new List<SpriteMetaData>();
+		//naked
+		metas.Add(Def0(0, 0, "body_white_nomouth"));
+		metas.Add(Def0(0, 1, "body_white_withmouth"));
+		metas.Add(Def0(1, 0, "body_yellow_nomouth"));
+		metas.Add(Def0(1, 1, "body_yellow_withmouth"));
+		metas.Add(Def0(2, 0, "body_brown_nomouth"));
+		metas.Add(Def0(2, 1, "body_brown_withmouth"));
+		metas.Add(Def0(3, 0, "body_green_nomouth"));
+		metas.Add(Def0(3, 1, "body_green_withmouth"));
+		//pants
+		AddDef(metas, "pants", 3, 0, 4, 1, 4, 2, 1);
+		//shoes
+		AddDef(metas, "pants", 4, 0, 4, 1, 4, 2, 1);
+		//bigshoes
+		AddDef(metas, "pants", 3, 4, 2, 2, 1, 2, 1);
+		//shirt
+		AddDef(metas, "shirt", 6, 0, 20, 4, 5, 6, 3);
+		//hair
+		AddDef(metas, "hair", 19, 0, 12, 4, 3, 5, 2);
+		//beard
+		AddDef(metas, "beard", 19, 3, 4, 4, 1, 4, 2);
+		//hat
+		AddDef(metas, "beard", 28, 0, 36, 4, 9, 1, 1);
+		//shield
+		AddDef(metas, "shield", 33, 0, 10, 4, 3, 6, 2);
+		//melee
+		AddDef(metas, "melee", 42, 0, 10, 5, 2, 10, 2);
+		//bow
+		AddDef(metas, "bow", 52, 0, 10, 2, 5, 1, 1);
+		TextureImporter imp = AssetImporter.GetAtPath("Assets" + tilesetPath) as TextureImporter;
+		imp.isReadable = true;
+		imp.spritesheet = metas.ToArray();
+		imp.spriteImportMode = SpriteImportMode.Multiple;
+		imp.spritePixelsPerUnit = SPRITE_SIZE;
+		AssetDatabase.ImportAsset("Assets" + tilesetPath, ImportAssetOptions.ForceUpdate);
+/*
+		names.Sort();
+		EditEnumText enun = new EditEnumText();
+		enun.name = "CharSpriteID";
+		enun.values = names;
+		EditEnumFile text = new EditEnumFile();
+		text.all.Add(enun);
+		string[] lines = text.SaveEnums();
+		const string textPath = "/Scripts/Schema/CharSpriteID.cs";
+		File.WriteAllLines(Application.dataPath + textPath, lines);
+*/
+		AssetDatabase.Refresh();
+	}
+}
+
